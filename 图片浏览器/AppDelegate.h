d98f6e8bdc65af64611a98c5bfaa8504fc24d5ae
Miles <596442958@qq.com>
2015-12-12 11:43:18 +0800

    Initial Commit

---
+++ 图片浏览器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片浏览器
+//
+//  Created by Miles on 12/12/15.
+//  Copyright © 2015 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片浏览器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片浏览器
+//
+//  Created by Miles on 12/12/15.
+//  Copyright © 2015 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
